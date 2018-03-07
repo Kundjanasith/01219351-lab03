06641e2e5aa44bce7f399f3e9c443171385c1141
Kjzth <kundjanasith.t@ku.th>
2018-03-07 16:31:32 +0700

    push tesata

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
 //         console.log(data);
 //     }
 //   });
-  console.log('ddd',$.getJSON('./questions.json',function(data){
+  console.log('ddd',$.getJSON('https://kundjanasith.github.io/01219351-lab03/data/questions.json',function(data){
     cosnole.log('data',data)
   }))
   console.log("Finish . . . ");
